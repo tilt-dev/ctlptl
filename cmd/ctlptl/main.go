de3f03c15d5612d4b82e7c37d54a28d9ddfa5b48
Nick Santos <nicholas.j.santos@gmail.com>
2020-11-04 18:18:24 -0500

    add release scripts for ctlptl (#32)

--- cmd/ctlptl/main.go
+++ cmd/ctlptl/main.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
+	"strings"
 
+	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"github.com/tilt-dev/ctlptl/pkg/cmd"
 	"k8s.io/klog/v2"
 )
 
+// Magic variables set by goreleaser
+var version string
+var commit string
+var date string
+
 func main() {
 	command := cmd.NewRootCommand()
+	command.AddCommand(newVersionCommand())
 
 	klog.InitFlags(nil)
 	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
@@ -18,4 +27,31 @@ func main() {
 	if err := command.Execute(); err != nil {
 		os.Exit(1)
 	}
+}
+
+func newVersionCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "version",
+		Short: "Current ctlptl version",
+		Run: func(_ *cobra.Command, args []string) {
+			fmt.Println(versionStamp())
+		},
+	}
+}
+
+func versionStamp() string {
+	timeIndex := strings.Index(date, "T")
+	if timeIndex != -1 {
+		date = date[0:timeIndex]
+	}
+
+	if date == "" {
+		date = "unknown"
+	}
+
+	if version == "" {
+		version = "0.0.0-master"
+	}
+
+	return fmt.Sprintf("v%s, built %s", version, date)
 }
