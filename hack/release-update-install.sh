365406cbb45822072e5bb5430119aa5638b059dd
Nick Santos <nick@tilt.dev>
2021-09-23 12:26:44 -0400

    hack: auto-update install instructions (#135)
    Co-authored-by: Tilt Dev <hi@tilt.dev>

---
+++ hack/release-update-install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Updates the Tilt repo with the latest version info.
+#
+# Usage:
+# scripts/update-tilt-repo.sh $VERSION
+# where VERSION is of the form 0.1.0
+
+set -euo pipefail
+
+if [[ "${GITHUB_TOKEN-}" == "" ]]; then
+    echo "Missing GITHUB_TOKEN"
+    exit 1
+fi
+
+VERSION=${1//v/}
+VERSION_PATTERN="^[0-9]+\\.[0-9]+\\.[0-9]+$"
+if ! [[ $VERSION =~ $VERSION_PATTERN ]]; then
+    echo "Version did not match expected pattern. Actual: $VERSION"
+    exit 1
+fi
+
+DIR=$(dirname "$0")
+cd "$DIR/.."
+
+ROOT=$(mktemp -d)
+git clone https://tilt-releaser:"$GITHUB_TOKEN"@github.com/tilt-dev/ctlptl "$ROOT"
+
+set -x
+cd "$ROOT"
+sed -i -E "s/CTLPTL_VERSION=\".*\"/CTLPTL_VERSION=\"$VERSION\"/" INSTALL.md
+sed -i -E "s/CTLPTL_VERSION = \".*\"/CTLPTL_VERSION = \"$VERSION\"/" INSTALL.md
+git add .
+git config --global user.email "hi@tilt.dev"
+git config --global user.name "Tilt Dev"
+git commit -a -m "Update version numbers: $VERSION"
+#git push origin master
+
+#rm -fR "$ROOT"
---
+++ hack/release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Do a complete release. Run on CI.
+
+set -ex
+
+if [[ "$GITHUB_TOKEN" == "" ]]; then
+    echo "Missing GITHUB_TOKEN"
+    exit 1
+fi
+
+if [[ "$DOCKER_TOKEN" == "" ]]; then
+    echo "Missing DOCKER_TOKEN"
+    exit 1
+fi
+
+DIR=$(dirname "$0")
+cd "$DIR/.."
+
+echo "$DOCKER_TOKEN" | docker login --username "$DOCKER_USERNAME" --password-stdin
+
+git fetch --tags
+goreleaser --rm-dist
+
+VERSION=$(git describe --abbrev=0 --tags)
+
+./scripts/release-update-tilt-repo.sh "$VERSION"
