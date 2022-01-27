01e22d885c99d6601bc38a5a5c2e4b31bb9084de
Nick Santos <nicholas.j.santos@gmail.com>
2022-01-26 20:52:40 -0500

    cluster: adjust connection settings for docker-desktop 4.4.4

--- pkg/cluster/docker_desktop_dial_windows.go
+++ pkg/cluster/docker_desktop_dial_windows.go
@@ -4,14 +4,29 @@ package cluster
 
 import (
 	"net"
+	"os"
+	"time"
 
 	"gopkg.in/natefinch/npipe.v2"
 )
 
 func dockerDesktopSocketPaths() ([]string, error) {
-	return []string{`\\.\pipe\dockerWebApiServer`}, nil
+	return []string{
+		`\\.\pipe\dockerWebApiServer`,
+		`\\.\pipe\dockerBackendNativeApiServer`,
+	}, nil
 }
 
+// Use npipe.Dial to create a connection.
+//
+// npipe.Dial will wait if the socket doesn't exist. Stat it first and
+// dial on a timeout.
+//
+// https://github.com/natefinch/npipe#func-dial
 func dialDockerDesktop(socketPath string) (net.Conn, error) {
-	return npipe.Dial(socketPath)
+	_, err := os.Stat(socketPath)
+	if err != nil {
+		return nil, err
+	}
+	return npipe.DialTimeout(socketPath, 2*time.Second)
 }
