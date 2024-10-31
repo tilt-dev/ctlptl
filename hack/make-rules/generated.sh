b597023084199c7ecedf691b5c79364c44f6fa1f
Nick Santos <nick.santos@docker.com>
2024-10-31 16:00:51 -0400

    hack: update deepcopy-gen
    Signed-off-by: Nick Santos <nick.santos@docker.com>

--- hack/make-rules/generated.sh
+++ hack/make-rules/generated.sh
@@ -6,15 +6,11 @@ REPO_ROOT=$(dirname $(dirname $(dirname "$0")))
 cd "${REPO_ROOT}"
 
 GOROOT="$(go env GOROOT)"
+rm -f pkg/api/*.deepcopy.go
+rm -f pkg/api/*/*.deepcopy.go
+go install k8s.io/code-generator/cmd/deepcopy-gen@v0.31.2
 deepcopy-gen \
-   -i "./pkg/api/k3dv1alpha4" \
-   -O zz_generated.deepcopy \
-   --go-header-file hack/boilerplate.go.txt
-deepcopy-gen \
-   -i "./pkg/api/k3dv1alpha5" \
-   -O zz_generated.deepcopy \
-   --go-header-file hack/boilerplate.go.txt
-deepcopy-gen \
-   -i "./pkg/api" \
-   -O zz_generated.deepcopy \
-   --go-header-file hack/boilerplate.go.txt
+   --go-header-file hack/boilerplate.go.txt \
+   ./pkg/api \
+   ./pkg/api/k3dv1alpha4 \
+   ./pkg/api/k3dv1alpha5
