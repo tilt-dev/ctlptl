161d07a8325d72277c445671843ddb677f36da48
Nick Santos <nicholas.j.santos@gmail.com>
2026-08-26 22:36:46 -0400

    vendor: update k8s deps to 1.37 (#422)
    also fix codegen
    
    Signed-off-by: Nick Santos <nicholas.j.santos@gmail.com>

--- hack/make-rules/generated.sh
+++ hack/make-rules/generated.sh
@@ -5,10 +5,13 @@ set -exuo pipefail
 REPO_ROOT=$(dirname $(dirname $(dirname "$0")))
 cd "${REPO_ROOT}"
 
-GOROOT="$(go env GOROOT)"
+# Keep the generator in lockstep with the k8s libraries we depend on,
+# so it never drifts far enough behind to stop building.
+K8S_VERSION=$(go list -m -f '{{.Version}}' k8s.io/api)
+
 rm -f pkg/api/*.deepcopy.go
 rm -f pkg/api/*/*.deepcopy.go
-go install k8s.io/code-generator/cmd/deepcopy-gen@v0.31.2
+go install "k8s.io/code-generator/cmd/deepcopy-gen@${K8S_VERSION}"
 deepcopy-gen \
    --go-header-file hack/boilerplate.go.txt \
    ./pkg/api \
